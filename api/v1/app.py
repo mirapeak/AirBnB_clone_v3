df0d95ee5d38c1db72b2b29a6aa0f6fe9a5d9be3
mirapeak <livinusmiracle2009@yahoo.com>
2023-05-02 03:53:21 +0000

    Update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,45 +1,29 @@
 #!/usr/bin/python3
 """
-app
+App module for init
 """
-
-from flask import Flask, jsonify
-from flask_cors import CORS
-from os import getenv
-
-from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
 from models import storage
-
+import os
+import sys
+from api.v1.views import app_views
 
 app = Flask(__name__)
-
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
 app.register_blueprint(app_views)
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 @app.teardown_appcontext
-def teardown(exception):
-    """
-    teardown function
-    """
+def tearDown(self):
+    """ TearDown the process """
     storage.close()
 
 
-@app.errorhandler(404)
-def handle_404(exception):
-    """
-    handles 404 error
-    :return: returns 404 json
-    """
-    data = {
-        "error": "Not found"
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 404
-
-    return(resp)
-
 if __name__ == "__main__":
-    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
+    app.run(host=os.getenv("HBNB_API_HOST"),
+            port=os.getenv("HBNB_API_PORT"), threaded=True)
+    sys.exit()
